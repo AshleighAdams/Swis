1d63b05c03cfd8caebe55e2b941cef50a503e4d4
Ashleigh Adams <ashleigh.k.adams@gmail.com>
2019-12-15 02:15:48 +0000

    Added unit tests.

---
+++ UnitTests/Util/ReinterpretCast.cs
@@ -0,0 +1,74 @@
+using System;
+using Swis;
+using Xunit;
+
+namespace UnitTests
+{
+	public class ReinterpretCastTests
+	{
+		[Theory]
+		[InlineData((short)-1)]
+		[InlineData((short)-123)]
+		[InlineData((short)123)]
+		[InlineData((short)0)]
+		[InlineData(short.MaxValue)]
+		[InlineData(short.MinValue)]
+		public void ReinterpretCastShortInt(short value)
+		{
+			var result = Util.ReinterpretCast<short, int>(value);
+			var original = Util.ReinterpretCast<int, short>(result);
+
+			Assert.Equal(value, original);
+
+			Caster c;
+			c.I32 = 0; c.U32 = 0;
+			c.I16A = value;
+			// registers don't see endianness, but memory (where Caster is used) does
+			uint caster_extended = Util.SignExtend(c.U32, 16);
+			uint result_uint = Util.ReinterpretCast<int, uint>(result);
+
+			Assert.Equal(caster_extended, result_uint);
+		}
+
+		[Theory]
+		[InlineData(-1)]
+		[InlineData(-123)]
+		[InlineData(123)]
+		[InlineData(0)]
+		[InlineData(int.MaxValue)]
+		[InlineData(int.MinValue)]
+		public void ReinterpretCastIntUInt(int value)
+		{
+			var result = Util.ReinterpretCast<int, uint>(value);
+			var original = Util.ReinterpretCast<uint, int>(result);
+
+			Assert.Equal(value, original);
+
+			Caster c; c.U32 = 0;
+			c.I32 = value;
+			
+			Assert.Equal(c.U32, result);
+		}
+
+		[Theory]
+		[InlineData(0.5f)]
+		[InlineData(-1.0f)]
+		[InlineData(-123.0f)]
+		[InlineData(123.0f)]
+		[InlineData(0.0f)]
+		[InlineData(float.MaxValue)]
+		[InlineData(float.MinValue)]
+		public void ReinterpretCastFloatUInt(float value)
+		{
+			var result = Util.ReinterpretCast<float, uint>(value);
+			var original = Util.ReinterpretCast<uint, float>(result);
+
+			Assert.Equal(value, original);
+
+			Caster c; c.U32 = 0;
+			c.F32 = value;
+
+			Assert.Equal(c.U32, result);
+		}
+	}
+}
---
+++ UnitTests/Util/SignExtend.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Swis;
+using Xunit;
+using FluentAssertions;
+
+namespace UnitTests
+{
+	public class SignExtendTests
+	{
+		[Theory]
+		[InlineData(0x000000ffu, 8u, 0xffffffffu)]
+		[InlineData(0x0000000fu, 8u, 0x0000000fu)]
+		[InlineData(0x0000000fu, 4u, 0xffffffffu)]
+		[InlineData(0x0000000fu, 5u, 0x0000000fu)]
+		[InlineData(0b00000000000000000000000000000000u, 1u,  0b00000000000000000000000000000000u)]
+		[InlineData(0b00000000000000000000000000000001u, 1u,  0b11111111111111111111111111111111u)]
+		[InlineData(0b00000000000000000101010101010101u, 15u, 0b11111111111111111101010101010101u)]
+		[InlineData(0b00000000000000000101010101010101u, 16u, 0b00000000000000000101010101010101u)]
+		[InlineData(0b00000000000000000101010101010101u, 6u,  0b00000000000000000000000000010101u)]
+		[InlineData(0b00000000000000000010101010101010u, 6u,  0b11111111111111111111111111101010u)]
+		[InlineData(0b00000000000000000101010101010101u, 7u,  0b11111111111111111111111111010101u)]
+		[InlineData(0b00000000000000000010101010101010u, 7u,  0b00000000000000000000000000101010u)]
+		public void CheckKnown(uint value, uint from_bits, uint expected)
+		{
+			Util.SignExtend(value, from_bits).Should().Be(expected);
+		}
+
+		[Theory]
+		[InlineData(1u, 0u)]
+		public void CheckThrows(uint value, uint from_bits)
+		{
+			Assert.Throws<ArgumentOutOfRangeException>(() => Util.SignExtend(value, from_bits));
+		}
+	}
+}
