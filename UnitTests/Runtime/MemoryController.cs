530216b3e1d45ee8581a4dea74687455dba632a6
Ashleigh Adams <ashleigh.k.adams@gmail.com>
2019-12-18 20:38:48 +0000

    Added some more memory controller tests.

--- UnitTests/Runtime/MemoryController.cs
+++ UnitTests/Runtime/MemoryController.cs
@@ -11,6 +11,7 @@ namespace UnitTests
 
 		[Theory]
 		[InlineData(0u, 8u, 0x1)]
+		[InlineData(3u, 16u, 0x0a00)]
 		[InlineData(0u, 32u, 0x1)]
 		[InlineData(2u, 32u, 0x0b0a0000)]
 		[InlineData(6u, 32u, 0xaabbccdd)]
@@ -29,6 +30,7 @@ namespace UnitTests
 
 		[Theory]
 		[InlineData(0u, 8u, 0x1, "\x01")]
+		[InlineData(3u, 16u, 0x0a00, "\x00\x0a")]
 		[InlineData(0u, 32u, 0x1, "\x01\x00\x00\x00")]
 		[InlineData(0u, 32u, 0xaabbccdd, "\xdd\xcc\xbb\xaa")]
 		[InlineData(2u, 32u, 0x0b0a0000, "\x00\x00\x0a\x0b")]
@@ -48,6 +50,56 @@ namespace UnitTests
 
 				for (int i = (int)addr, n = 0; i < bits / 8; i++, n++)
 					controller.Memory[i].Should().Be((byte)result[n]);
+			}
+		}
+
+		[Theory]
+		[InlineData(10)]
+		[InlineData(11)]
+		[InlineData(100)]
+		public void OutOfIndex(uint addr)
+		{
+			{
+				var controller = new ByteArrayMemoryController(new byte[10]);
+				Assert.Throws<IndexOutOfRangeException>(() => controller[addr]);
+			}
+			{
+				var controller = new PointerMemoryController(new byte[10]);
+				Assert.Throws<IndexOutOfRangeException>(() => controller[addr]);
+			}
+		}
+
+		[Theory]
+		[InlineData(10, 8)]
+		[InlineData(11, 32)]
+		[InlineData(uint.MaxValue, 32)]
+		[InlineData(9, 16)]
+		[InlineData(7, 32)]
+		public void OutOfIndexSize(uint addr, uint size)
+		{
+			{
+				var controller = new ByteArrayMemoryController(new byte[10]);
+				Assert.Throws<IndexOutOfRangeException>(() => controller[addr, size]);
+			}
+			{
+				var controller = new PointerMemoryController(new byte[10]);
+				Assert.Throws<IndexOutOfRangeException>(() => controller[addr, size]);
+			}
+		}
+
+		[Theory]
+		[InlineData(9, 8)]
+		[InlineData(8, 16)]
+		[InlineData(6, 32)]
+		public void InsideOfIndexSize(uint addr, uint size)
+		{
+			{
+				var controller = new ByteArrayMemoryController(new byte[10]);
+				_ = controller[addr, size];
+			}
+			{
+				var controller = new PointerMemoryController(new byte[10]);
+				_ = controller[addr, size];
 			}
 		}
 	}
