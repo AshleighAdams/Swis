9296215eaeb40264e7587dc2bd26e79d3c2ac923
Ashleigh Adams <ashleigh.k.adams@gmail.com>
2020-04-27 23:25:49 +0100

    WpfDebugger now runs on .NET Core 3.1. Replaced Scintilla.NET with a Core compatible version.

---
+++ Swis.WpfDebugger/Source/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Swis.WpfDebugger
+{
+	static class Program
+	{
+		[STAThread]
+		static void Main()
+		{
+			var app = new App();
+			app.Run(new MainWindow());
+		}
+	}
+}
